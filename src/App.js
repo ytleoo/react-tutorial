587cdbec3e3d823845c8db353e080bc37d44a2c0
Saya Kubota <3yata.k.ar@gmail.com>
2024-05-11 11:15:32 +0900

    initial
    initial
    
    ボタン作成
    
    gitignore
    
    state追加
    
    :fire:
    
    state更新
    
    update

---
+++ src/App.js
@@ -0,0 +1,38 @@
+import { useState } from 'react';
+
+function Square() {
+  const [value, setValue] = useState(null);
+
+  function handleClick() {
+    setValue('X');
+  }
+
+  return <button 
+    className="square"
+    onClick={handleClick}
+  >
+    { value }
+  </button>
+}
+
+export default function  Board() {
+  return (
+    <>
+      <div className="board-row">
+        <Square />
+        <Square />
+        <Square />
+      </div>
+      <div className="board-row">
+        <Square />
+        <Square />
+        <Square />
+      </div>
+      <div className="board-row">
+        <Square />
+        <Square />
+        <Square />
+      </div>
+    </>
+  )
+}
